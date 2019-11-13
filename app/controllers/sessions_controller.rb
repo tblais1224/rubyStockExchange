6e7585cd7798c9d2e9afcc3f864d763989410f4b
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-13 14:23:18 -0500

    user signup login and logout working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:passowrd])
-      sessions[:user_id] = @user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
 
       redirect_to '/welcome'
     else
@@ -22,6 +22,12 @@ class SessionsController < ApplicationController
     
   end
   
+  def logout
+    
+    session.delete(:user_id)
+    redirect_to '/welcome'
+
+  end
 
   def login
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191113165009) do
+ActiveRecord::Schema.define(version: 20191113184213) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
